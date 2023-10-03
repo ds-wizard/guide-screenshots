5f7e29b626c46090a64fa49988de77a34d5c43fe
Jan Slifka <jan.slifka@gmail.com>
2023-10-03 13:26:57 +0200

    Improve active sessions screenshot

--- cypress/e2e/active-sessions.cy.js
+++ cypress/e2e/active-sessions.cy.js
@@ -2,7 +2,22 @@ describe('Profile', () => {
     it('Active Sessions', () => {
         cy.loginAs('admin')
         cy.visitApp('/users/edit/current/active-sessions')
+        cy.contains('Revoke all').click()
+        cy.clickModalAction()
 
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('apiUrl') + '/tokens',
+            body: { 
+                email: Cypress.env('adminUsername'),
+                password: Cypress.env('adminPassword')
+             },
+             headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+             }
+        })
+
+        cy.visitApp('/users/edit/current/active-sessions')
         cy.get('.col-wide-detail').screenshot('application/profile/edit/active-sessions/form', { padding: [50, 240, 10, 330] })
     })
 })

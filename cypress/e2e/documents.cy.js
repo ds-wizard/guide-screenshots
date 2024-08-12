88259945045d8158cacfebc057215397a759e934
jmartinkova <j.martinkova@outlook.com>
2024-08-12 08:22:49 +0200

    Refactor Documents

--- cypress/e2e/documents.cy.js
+++ cypress/e2e/documents.cy.js
@@ -4,8 +4,6 @@ describe('Documents', () => {
         cy.visitApp('/documents')
         cy.get('.list-group-item').should('exist')
 
-        cy.wait(500)
-
         cy.get('.dropdown-toggle').last().click()
         cy.get('.dropdown-item').should('be.visible')
 

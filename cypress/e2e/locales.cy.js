e9580c96b4a019175e030590174262359029f9e0
jmartinkova <j.martinkova@outlook.com>
2024-08-12 08:49:45 +0200

    Refactor Locales

--- cypress/e2e/locales.cy.js
+++ cypress/e2e/locales.cy.js
@@ -4,8 +4,6 @@ describe('Locales', () => {
         cy.visitApp('/locales')
         cy.get('.list-group-item').should('exist')
 
-        cy.wait(500)
-
         cy.get('.dropdown-toggle').last().click()
         cy.get('.dropdown-item').should('be.visible')
 

fb384b929ee606347171f0902aff92ab5187bd35
jmartinkova <j.martinkova@outlook.com>
2024-08-12 08:19:15 +0200

    Refactor Document-templates

--- cypress/e2e/document-templates.cy.js
+++ cypress/e2e/document-templates.cy.js
@@ -4,8 +4,6 @@ describe('Document Templates', () => {
         cy.visitApp('/document-templates')
         cy.get('.list-group-item').should('exist')
 
-        cy.wait(500)
-
         cy.get('.dropdown-toggle').last().click()
         cy.get('.dropdown-item').should('be.visible')
 

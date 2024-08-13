a944b8386557025fd8a5031e4df8507c4994fadc
jmartinkova <j.martinkova@outlook.com>
2024-08-13 09:22:26 +0200

    Refactor Users

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -4,8 +4,6 @@ describe('Users', () => {
         cy.visitApp('/users')
         cy.get('.list-group-item').should('exist')
 
-        cy.wait(500)
-
         cy.get('.dropdown-toggle').last().click()
         cy.get('.dropdown-item').should('be.visible')
 
